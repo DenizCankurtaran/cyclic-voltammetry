683a15a42ccc103c32dd0b34854f09aadd872307
Florian Göhrs <goehrs.florian@gmail.com>
2024-06-12 14:53:18 +0200

    navbar component with bug

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st
+from streamlit_navigation_bar import st_navbar
 
+page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
+st.write(page)
 st.title('Cyclic Voltammetry')
