5286040efa4bb38e4480dd3756f40915df3d73e1
Mike Kanik <mike.kanik@mail.de>
2024-06-23 12:23:21 +0200

    graphdetail page prep

--- pages/3_graphdetail.py
+++ pages/3_graphdetail.py
@@ -8,40 +8,17 @@ if "multiplot" not in st.session_state:
     st.session_state["multiplot"] = []
 
 entries = st.session_state["multiplot"]
+
 st.set_page_config(layout="wide")
 st.title("Chart")
+col1, col2 = st.columns([3, 2])
 
-normalize_checkbox = st.checkbox("Normalize", True)
-normalize_electrolyte_checkbox = st.checkbox("Normalize Electrolyte Concentration")
-normalize_scan_rate_checkbox = st.checkbox("Normalize Scan Rate")
+with col1:
+    plot_graph(entries, "Detail Entries")
 
-ref_electrode = None
-c_ref = None
-ref_scan_rate = None
-
-if normalize_checkbox:
-    ref_electrode = st.selectbox(
-        "Select Reference Electrode",
-        [
-            "Ag/AgCl",
-            "Ag/AgCl-sat",
-            "Ag/AgCl_3M",
-            "Hg/HgO/0.1 M NaOH",
-            "RHE",
-            "SCE",
-            "wire",
-            "SHE",
-            "NCE",
-        ],
-        index=7,
-    )
-
-if normalize_electrolyte_checkbox:
-    c_ref = 1.0
-
-if normalize_scan_rate_checkbox:
-    ref_scan_rate = 1.0 
-
-normalized_entries = normalize_entries(entries, ref_electrode, c_ref, ref_scan_rate)
-
-plot_graph(normalized_entries, "Normalized Entries")
+with col2:
+    data = {
+        "Property": ["Placeholder 1", "Placeholder 2", "Placeholder 3"],
+        "Value": ["aa", "aa", "Ag"],
+    }
+    st.table(data)
