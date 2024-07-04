dac843512165bf82749c32d2a271c682cf210ca4
Florian Göhrs <goehrs.florian@gmail.com>
2024-07-04 11:31:57 +0200

    added color

--- components/periodic_buttons.py
+++ components/periodic_buttons.py
@@ -124,11 +124,21 @@ button_colors = {
 
 def PeriodicTable():
     st.title("Interactive Periodic Table")
+
     
+       
     # Layout the first row
     col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16, col17, col18 = st.columns(18)
     
     with col1:
+        st.markdown("""
+            <style>.element-container:has(#button-after) + div button {
+            background-color: rgb(204, 49, 49);
+                    width: 40px;
+                    height: 40px;
+            }</style>""", unsafe_allow_html=True)
+        
+        st.markdown('<span id="button-after"></span>', unsafe_allow_html=True)
         st.button("H", on_click=select_element, args=("H",))
     with col18:
         st.button("He", on_click=select_element, args=("He",))
@@ -334,8 +344,8 @@ def PeriodicTable():
         st.button("Og", on_click=select_element, args=("Og",))
 
 # Apply the colors to the buttons
-    for element, colors in button_colors.items():
-        ChangeButtonColour(element, colors["font_color"], colors["background_color"])    
+   # for element, colors in button_colors.items():
+    #    ChangeButtonColour(element, colors["font_color"], colors["background_color"])    
  
 
 
